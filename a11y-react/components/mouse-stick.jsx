2fca95f75d580837f4ffd5dc4a4ffd39ace6c2cf
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-08-11 09:20:56 +0800

    update image

--- a11y-react/components/mouse-stick.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Image } from 'rebass';
-import img from './mouse-stick.jpg';
-
-export default function MouseStick() {
-  return <Image src={img} height="95vh" />;
-}
--- a11y-react/components/screen-reader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Image } from 'rebass';
-import img from './screen-reader.jpg';
-
-export default function ScreenReader() {
-  return <Image src={img} height="90vh" />;
-}
