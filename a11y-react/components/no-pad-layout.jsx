d68e7bd1cd659e7f2754b22e1f111550896f24a3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-08-12 10:39:58 +0800

    update slide

---
+++ a11y-react/components/no-pad-layout.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const NoPadLayout = styled.div`
+  padding: 0;
+  margin: 0;
+`;
+
+export default NoPadLayout;
--- a11y-react/components/type-of-disability.jsx
+++ a11y-react/components/type-of-disability.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import { Image } from 'rebass';
+import { Image, Flex, Box } from 'rebass';
 import img from './type-of-disability.png';
 
 export default function TypeofDisability() {
-  return <Image src={img} height="95vh" />;
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      style={{
+        height: '100%',
+      }}
+    >
+      <Box>
+        <Image src={img} />
+      </Box>
+    </Flex>
+  );
 }
